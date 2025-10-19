22941927596a334b38093c44d23d9b49cee2b234
Mykhailo Pyroh <mykhailopk@gmail.com>
2025-10-19 21:51:03 +0200

    ready

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,46 +1,34 @@
-const form = document.querySelector('.feedback-form');
-const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector(`.feedback-form`);
+const textarea = form.querySelector(`textarea`);
+const input = form.querySelector(`input`);
+const STORAGE_KEY = "feedback-form-state";
 
-let formData = {
-  email: '',
-  message: '',
-};
+form.addEventListener("input", onFormInput);
+form.addEventListener("submit", handlerSubmit);
 
+populateForm();
 
-const savedData = localStorage.getItem(STORAGE_KEY);
-if (savedData) {
-  try {
-    const parsedData = JSON.parse(savedData);
-    formData = { ...formData, ...parsedData }; 
+function onFormInput() {
+  const formData = {
+    email: input.value,
+    message: textarea.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
-    if (formData.email) form.elements.email.value = formData.email;
-    if (formData.message) form.elements.message.value = formData.message;
-  } catch (err) {
-    console.warn('Помилка при читанні даних з localStorage:', err);
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    const {email, message} = JSON.parse(savedData)
+    input.value = email;
+    textarea.value = message;
   }
 }
 
+function handlerSubmit(event){
+  event.preventDefault();
 
-form.addEventListener('input', e => {
-  if (e.target.name === 'email' || e.target.name === 'message') {
-    formData[e.target.name] = e.target.value.trim();
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  }
-});
-
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  if (!formData.email || !formData.message) {
-    alert('Fill please all fields');
-    return;
-  }
-
-  console.log(' Form submitted:', formData);
-
-
-  form.reset();
-  localStorage.removeItem(STORAGE_KEY);
-  formData = { email: '', message: '' };
-});
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY)
+}
